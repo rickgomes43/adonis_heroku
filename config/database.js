02214f83bbc5cf1bd0cb229d2b73c5099b255d8d
Ricardo Gomes <contato@rickgomes.com.br>
2019-11-29 11:16:47 -0300

    Teste

--- config/database.js
+++ config/database.js
@@ -72,8 +72,8 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      host: Env.get('DB_HOST', DATABASE_URL.localhost),
-      port: Env.get('DB_PORT', ''),
+      host: Env.get('DB_HOST', DATABASE_URL.hostname),
+      port: Env.get('DB_PORT', DATABASE_URL.port),
       user: Env.get('DB_USER', DATABASE_URL.username),
       password: Env.get('DB_PASSWORD', DATABASE_URL.password),
       database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
